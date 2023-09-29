648ed34710a30b95e0533fd66e2324d9e0effea9
ivan <ivan291194@gmail.com>
2023-09-28 20:38:09 -0600

    added ball and ball movement without collisions

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-#20.49 ?v=vVGTZlnnX3U
+
 
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -12,6 +12,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
 
 class Paddle:
     
@@ -35,12 +36,37 @@ class Paddle:
         else:
             self.y += self.VEL
 
-def draw(win, paddles):
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+def draw(win, paddles, ball):
     win.fill(BLACK)
     
     for paddle in paddles:
         paddle.draw(win)
-    
+    #draw the dashed line in the middle
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+
+    ball.draw(win)
     pygame.display.update()
     
 def handle_paddle_movement(keys, left_paddle, right_paddle):
@@ -69,9 +95,11 @@ def main():
     # substract the margin + the paddle width to know where to start drawing
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
+    ball = Ball(WIDTH// 2, HEIGHT // 2, BALL_RADIUS )
+
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WIN, [left_paddle, right_paddle], ball)
         
         
         for event in pygame.event.get():
@@ -80,6 +108,9 @@ def main():
                 break
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
+
+        ball.move()
+
     pygame.quit()
 
 
